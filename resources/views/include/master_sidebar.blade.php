988d2b08cae87415330df1296a4b6c969ccf042a
biplob192 <biplob.net2@gmail.com>
2023-03-28 12:03:07 +0600

    added inspector and category index page

--- resources/views/include/master_sidebar.blade.php
+++ resources/views/include/master_sidebar.blade.php
@@ -103,6 +103,33 @@
                         </p>
                     </a>
                 </li> --}}
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tools"></i>
+                        <p>
+                            Settings
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{route('categories.index')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>
+                                    Category
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('inspectors.index')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>
+                                    Inspector
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 @endrole
             </ul>
         </nav>
